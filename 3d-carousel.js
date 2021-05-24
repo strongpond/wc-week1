dfa1bc837caabfb8fcb7966626174ca2c0b1d603
euiyeonlee <dmldus1121@naver.com>
2021-05-24 19:24:14 +0900

    favorites spin change

--- 3d-carousel.js
+++ 3d-carousel.js
@@ -1,6 +1,34 @@
 var angle = 0;
 function galleryspin(sign) { 
-spinner = document.querySelector("#spinner");
-if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
-spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+    if (document.getElementById("activityPhoto").style.display !=='none') {
+        if (!sign) { 
+            angle = angle + 90; } 
+        else { 
+            angle = angle - 90; }
+        spinner90.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+    }
+
+    else if (document.getElementById("travelPhoto").style.display !=='none') {
+        if (!sign) { 
+            angle = angle + 90; } 
+        else { 
+            angle = angle - 90; }
+        spinner90.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+    }
+
+    else if (document.getElementById("volunteerPhoto").style.display !=='none') {
+        if (!sign) { 
+            angle = angle + 60; } 
+        else { 
+            angle = angle - 60; }
+        spinner60.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+    }
+
+    else {
+        if (!sign) { 
+            angle = angle + 45; } 
+        else { 
+            angle = angle - 45; }
+        spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+    }
 }
